d880dbad2177a4d4d84bf4215960c4ec6fa68f28
alexsan11 <alexsan11@hotmail.com>
2016-06-25 08:47:40 -0300

    Initial test submission

---
+++ Queiroz.LojaVirtual.UnitTest/UnitTestQueiroz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Queiroz.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestQuerioz
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Queiroz.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Queiroz.LojaVirtual.Web.MvcApplication" Language="C#" %>
