370d36327e98ae3f271eb4434c781e2a6d2a7554
alexsan11 <alexsan11@hotmail.com>
2016-06-25 11:11:43 -0300

    Create Repository

---
+++ Queiroz.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Queiroz.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queiroz.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        //public DbSet<Categoria> Produtos { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+    }
+}
