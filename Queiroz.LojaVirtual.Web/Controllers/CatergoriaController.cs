fca21416cb7d8da008c5e92e9deedae13f88fed4
alexsan11 <alexsan11@hotmail.com>
2016-07-10 12:43:37 -0300

    Categoria

---
+++ Queiroz.LojaVirtual.Web/Controllers/CatergoriaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Queiroz.LojaVirtual.Dominio.Repositorio;
+
+namespace Queiroz.LojaVirtual.Web.Controllers
+{
+    public class CatergoriaController : Controller
+    {
+        ProdutosRepositorio _reporsitorio;
+        // GET: Catergoria
+        public PartialViewResult Menu(string categoria = null)
+        {
+            ViewBag.CategoriaSelecionada = categoria;
+
+            IEnumerable<string> categorias = _reporsitorio.Produtos
+                                                            .Select(c => c.Categoria)
+                                                            .Distinct()
+                                                            .OrderBy(c => c);
+            return PartialView(categorias);
+        }
+    }
+}
